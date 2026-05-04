8e5df33b8c40c7333cb6d59c0af76b6bdff88c4a
a3ubakarr <abubakarkhokhar077@gmail.com>
2026-05-04 22:18:50 +0500

    Added Python code file

---
+++ main.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello! This is my OSSD Assignment using Git and GitHub.")
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    greet()
+    
+    num1 = 5
+    num2 = 10
+    result = add_numbers(num1, num2)
+    
+    print("Sum is:", result)
